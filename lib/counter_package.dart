1e2de97725b9f8235380e26a0ee76839e9a62178
Bored Nerdy <theborednerdy@gmail.com>
2023-02-22 22:51:25 +0300

    initial commit

---
+++ lib/counter_package.dart
@@ -0,0 +1,15 @@
+library counter_package;
+
+class Counter {
+  int _count = 0;
+
+  int get count => _count;
+
+  void increment() {
+    _count++;
+  }
+
+  void decrement() {
+    _count--;
+  }
+}
